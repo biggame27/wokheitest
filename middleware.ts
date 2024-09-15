c982b7cb7e32b9432efbaa9c71dd7a66038297fb
biggame27 <koharrisonko@gmail.com>
2024-09-15 01:25:03 -0500

    user authentication fix

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
-import { authMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  ignoredRoutes: ['/api/webhooks(.*)'],
-})
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
